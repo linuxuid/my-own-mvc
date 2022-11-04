b852a0594ecdbb1bb67e499caf005a71edfa58ef
linuxuid <maxhaxclass@yandex.ru>
2022-11-04 20:37:36 +0300

    Created-Architecture-of-Project

---
+++ app/Controllers/MainPageController/MainPageController.php
@@ -0,0 +1,12 @@
+<?php 
+declare(strict_types=1);
+
+namespace App\Controllers\MainPageController;
+
+class MainPageController 
+{
+    public function index(): ?string
+    {
+        return 'hello';
+    }
+}
